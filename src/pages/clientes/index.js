3086972f26e9c99ff89efe7f672e486dd52aae56
CarlosRenatoCardoso <127361048+CarlosRenatoCardoso@users.noreply.github.com>
2023-09-26 22:55:47 -0300

    :sparkles: Criando paginas e rotas

---
+++ src/pages/clientes/index.js
@@ -0,0 +1,11 @@
+function Cliente () {
+    return (
+        <div>
+            <h1>
+                Cliente em construção!!!
+            </h1>
+        </div>
+    )
+}
+
+export default Cliente;
